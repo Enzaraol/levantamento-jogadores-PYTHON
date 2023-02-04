8b16f74b26a915d067672973f67b13b74add95b1
Enzaraol <108746947+Enzaraol@users.noreply.github.com>
2023-02-04 20:18:14 -0300

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-salario = float(input('Digite seu salário: '))
-valor = float(input('Digite o valor da casa: '))
-anos = int(input('Em quantos anos pretende pagar por ela? '))
-base = valor/(anos*12)
-if salario*0.3 > base:
-    print('\033[4;33mAPROVADO!\033[m')
-    print(f'O valor a pagar será de R${base:.2f} durante {anos} anos!')
-else:
-    print('NEGADO!')
